a6d1306d4135b3d2d59171dea26f0f6ab303b609
Moritz Lawitschka <me@moritzlawitschka.de>
2015-10-25 12:02:17 +0100

    Ensure jQuery Backstretch is loaded

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-backstretch'
+  name: 'ember-cli-backstretch',
+
+  included: function(app) {
+    this._super.included(app);
+
+    app.import(app.bowerDirectory + '/jquery-backstretch/jquery.backstretch.js');
+  }
 };
